f28bb0b357def9d847a645bed2f36900fe0f8e62
Victor Hugo <hugovco511@gmail.com>
2024-04-01 20:13:42 -0300

    feat: password do user não é retornada ao criar e atualizar um user

--- src/modules/users/dto/response-user.dto.ts
+++ src/modules/users/dto/response-user.dto.ts
@@ -2,6 +2,7 @@ import { UserEntity } from "../entities/users.entity";
 
 export class ResponseUser {
     constructor(user: UserEntity) {
+        delete user.password
         delete user.deletedAT
 
         return user
