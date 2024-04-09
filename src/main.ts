1a919c2503d7e459f79f61bb9780058c8fa88f6c
Victor Hugo <hugovco511@gmail.com>
2024-04-09 16:17:55 -0300

    feat: variável de ambiente para a porta da API

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT_API')
+  
+  if (!port) throw new Error("Application port wasn't found")
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -45,6 +50,6 @@ async function bootstrap() {
     },
   })
   
-  await app.listen(3000)
+  await app.listen(port)
 }
 bootstrap();
